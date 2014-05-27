38694856a4e0560c2b094b145482ef9b10a02d4c
jaketrent <trent.jake@gmail.com>
2014-05-26 22:37:17 -0600

    rm app

--- app/config/routes.js
+++
@@ -1,7 +0,0 @@
-const index = require('../common/controllers/index')
-const songs = require('../songs/songs-ctrl')
-
-exports.map = function (app) {
-  app.get('/', index.index)
-  app.get('/api/v1/songs', songs.index)
-}
--- app/static/js/client.js
+++
@@ -1,6 +0,0 @@
-//= require jquery/jquery.js
-//= require handlebars/handlebars.js
-//= require ember/ember.js
-//= require ember-data/ember-data.js
-
-Ember.Application.create()
